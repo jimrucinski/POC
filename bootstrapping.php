55e791cc4492daba71c8f37f34184d20aeec2280
jimrucinski <jimrucinski@msn.com>
2017-05-05 15:29:13 -0400

    demo

---
+++ bootstrapping.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+     <div class="container">
+         <h1>My First Bootstrap Page</h1>
+         <p>This is some text.</p>
+     </div>
+     <p>bootstrap is set to go 12 columns wide.</p>
+     <h2>This is a three columns with the col-sm-4 class on each div</h2>
+     <div class="row">
+         <div class="col-sm-4">Lorem ipsum dolor sit amet.</div>
+         <div class="col-sm-4">Consequatur nesciunt necessitatibus debitis omnis.</div>
+         <div class="col-sm-4">Ratione quae explicabo dolore neque.</div>
+     </div>
+     <h2>This is a a two unequal columns one with col-sm-4 and col-sm-8</h2>
+     <div class="row">
+         <div class="col-sm-4">col-sm-4</div>
+         <div class="col-sm-8"><mark>col-sm-8</mark></div>
+     </div>
+     <p class="text-muted">this is using the class text-muted</p>
+     <p class="text-primary">this is using the class text-primary</p>
+     <p class="text-success">this is using the class text-success</p>
+     <p class="text-info">this is using the class text-info</p>
+     <p class="text-warning">this is using the class text-warning</p>
+     <p class="text-danger">this is using the class text-danger</p>
+     <p class="bg-primary">this is using the class bg-primary</p>
+     <p class="bg-success">this is using the class bg-success</p>
+     <p class="bg-info">this is using the class bg-info</p>
+     <p class="bg-warning">this is using the class bg-warning</p>
+     <p class="bg-danger">this is using the class bg-danger</p>
+     <p>Resize this page to see how the text below changes:</p>
+     <h1 class="hidden-xs bg-danger">This text is hidden on an EXTRA SMALL screen.</h1>
+     <h1 class="hidden-sm bg-info">This text is hidden on a SMALL screen.</h1>
+     <h1 class="hidden-md bg-warning">This is text hidden on a MEDIUM screen.</h1>
+     <h1 class="hidden-lg bg-success">This is text hidden on a LARGE screen.</h1>
+     <h1 class="visible-xs">This text is shown only on an EXTRA SMALL screen.</h1>
+     <h1 class="visible-sm">This text is shown only on a SMALL screen.</h1>
+     <h1 class="visible-md">This text is shown only on a MEDIUM screen.</h1>
+     <h1 class="visible-lg">This text is shown only on a LARGE screen.</h1>
+     <div class="col-sm-3">image uses the img-rounded class<img src="black-spider-monkey.jpg" class="img-rounded"/></div>
+     <div class="col-sm-3">image uses the img-circle class<img src="black-spider-monkey.jpg" class="img-circle"/></div>
+     <div class="col-sm-3">image uses the img-thumbnail class<img src="black-spider-monkey.jpg" class="img-thumbnail"/></div>
+     <div class="col-sm-3">image uses the img-responsive class<img src="black-spider-monkey.jpg" class="img-responsive"/></div>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>nbr</th>
+                    <th>Firtname</th>
+                    <th>Lasname</th>
+                    <th>Age</th>
+                    <th>Country</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Anna</td>
+                    <td>Pitt</td>
+                    <td>35</td>
+                    <td>USA</td>
+                </tr>
+            </thead>
+        </table>
+    </div>
